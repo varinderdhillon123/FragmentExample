8d5255c0f8018d028ff910967fa979bfa62eff4b
macstudent <varinder701dhillon>
2019-11-28 19:45:27 -0500

    hi

--- app/src/main/java/com/example/fragmentexample/RedFragment.java
+++ app/src/main/java/com/example/fragmentexample/RedFragment.java
@@ -11,6 +11,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
@@ -27,6 +28,8 @@ public class RedFragment extends Fragment {
     private static final String ARG_PARAM1 = "param1";
     private static final String ARG_PARAM2 = "param2";
 
+    private Button btnSetData;
+
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
@@ -37,14 +40,7 @@ public class RedFragment extends Fragment {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment RedFragment.
-     */
+
     // TODO: Rename and change types and number of parameters
     public static RedFragment newInstance(String param1, String param2) {
         RedFragment fragment = new RedFragment();
@@ -92,6 +88,14 @@ public class RedFragment extends Fragment {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+
+        btnSetData = view.findViewById(R.id.btnSetTitle);
+        btnSetData.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                onButtonPressed("Hello from RED");
+            }
+        });
         // Get the ids of views here
 
     }
@@ -102,16 +106,7 @@ public class RedFragment extends Fragment {
         mListener = null;
     }
 
-    /**
-     * This interface must be implemented by activities that contain this
-     * fragment to allow an interaction in this fragment to be communicated
-     * to the activity and potentially other fragments contained in that
-     * activity.
-     * <p>
-     * See the Android Training lesson <a href=
-     * "http://developer.android.com/training/basics/fragments/communicating.html"
-     * >Communicating with Other Fragments</a> for more information.
-     */
+
     public interface OnFragmentInteractionListener {
         // TODO: Update argument type and name
         void onFragmentInteraction(Uri uri);
